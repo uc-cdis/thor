a2231068a23eed2f4c5012b86c8fbb35f68edddd
Marcelo Costa <themarcelor@gmail.com>
2021-08-21 15:36:08 -0500

    add jira==2.0 as a dependency and tweak scripts

--- jenkins-jobs-scripts/step1/create-release-in-jira.sh
+++ jenkins-jobs-scripts/step1/create-release-in-jira.sh
@@ -5,7 +5,6 @@ export JIRA_SVC_ACCOUNT="ctds.qa.automation@gmail.com"
 pip install --upgrade pip
 export CRYPTOGRAPHY_DONT_BUILD_RUST=1
 
-python3 -m pip install jira==2.0 --user
-
-python3 gen3release-sdk/create_jira_project_version.py
+poetry install
+poetry run python3 jenkins-jobs-scripts/step1-create-release-in-jira.py
 
