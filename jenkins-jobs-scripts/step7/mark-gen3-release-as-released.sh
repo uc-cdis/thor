d0b7ef210b4e21ce1bef0daeda90dbaef5924d89
Marcelo Costa <themarcelor@gmail.com>
2021-08-21 17:56:37 -0500

    fix py script path

--- jenkins-jobs-scripts/step7/mark-gen3-release-as-released.sh
+++ jenkins-jobs-scripts/step7/mark-gen3-release-as-released.sh
@@ -4,4 +4,4 @@ export JIRA_SVC_ACCOUNT="ctds.qa.automation@gmail.com"
 
 poetry install
 
-poetry run python3 gen3release-sdk/update_jira_release.py
+poetry run python3 jenkins-jobs-scripts/step7/mark-gen3-release-as-released.py
