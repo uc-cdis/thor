89dda234acc87a0feaaee395081595f03ac3e60b
Marcelo Costa <themarcelor@gmail.com>
2021-08-21 15:20:08 -0500

    adding step 1 scripts

---
+++ jenkins-jobs-scripts/step1/create-release-in-jira.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export JIRA_SVC_ACCOUNT="ctds.qa.automation@gmail.com"
+
+export CRYPTOGRAPHY_DONT_BUILD_RUST=1
+
+python3 -m pip install jira==2.0 --user
+
+python3 gen3release-sdk/create_jira_project_version.py
+
