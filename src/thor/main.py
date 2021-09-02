e3fe51c7c205a421e799734b0c87dbe6f367005a
colinyaos <89102198+colinyaos@users.noreply.github.com>
2021-09-02 10:08:08 -0500

    Renamed some methods in main.py to make them more distinct. Caught a nasty error with get_release_task being defined twice for different functionality, which would have been bad.

--- src/thor/main.py
+++ src/thor/main.py
@@ -7,7 +7,7 @@ from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
-from dao.release_dao import read_release, get_rel_keys
+from dao.release_dao import read_release, get_release_keys
 from dao.task_dao import read_task, get_task_keys
 
 # from dao.models import Task
@@ -19,10 +19,10 @@ app = FastAPI()
 
 
 @app.get("/releases")
-async def get_releases():
+async def get_all_releases():
     """ Returns all the releases in the Releases table. """
     releases_to_return = []
-    key_list = get_rel_keys()
+    key_list = get_release_keys()
     for key in key_list:
         release = jsonable_encoder(read_release(key))
         log.info(f"Successfully obtained release instance from DAO layer: {release}")
@@ -40,7 +40,7 @@ async def get_single_release(release_id):
 
 
 @app.get("/releases/{release_id}/tasks")
-async def get_release_tasks(release_id: int):
+async def get_all_release_tasks(release_id: int):
     """ This returns all tasks with release_id corresponding to the given input. 
     Currently, it fails without support if release_id is not an int, so this should
     be addressed when appropriate. """
@@ -55,7 +55,7 @@ async def get_release_tasks(release_id: int):
 
 
 @app.get("/releases/{release_id}/tasks/{task_id}")
-async def get_release_tasks(release_id: int, task_id: int):
+async def get_release_task_specific(release_id: int, task_id: int):
     """ This returns all tasks with release_id corresponding to the given input, and 
     task_id corresponding to the given input. Task_id is theoretically unique for each
     task, so this method is somewhat superfluous, but it's here if needed.  
@@ -73,7 +73,7 @@ async def get_release_tasks(release_id: int, task_id: int):
 
 
 @app.get("/tasks")
-async def get_tasks():
+async def get_all_tasks():
     """ Returns all the tasks in the Tasks table. """
     tasks_to_return = []
     key_list = get_task_keys()
