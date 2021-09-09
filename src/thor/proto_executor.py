d67c8e0befa23f0cbc2486245a66f7b79bdd829d
colinyaos <89102198+colinyaos@users.noreply.github.com>
2021-09-09 10:39:19 -0500

    Change proto_executor to work with cygwin's file structure. Now imports os.path to build an absolute path.

--- src/thor/proto_executor.py
+++ src/thor/proto_executor.py
@@ -4,9 +4,14 @@
 # work with and call.
 # Will try to execute python read/write to sample files.
 
+import os.path
+
 import datetime as dt
 
+save_path = "/cygdrive/c/users/henry/work/thor/thor/src/thor"
 log_file_name = "proto_log.csv"
+
+log_file_absolute_path = os.path.join(save_path, log_file_name)
 
 
 def method1():
@@ -14,7 +19,7 @@ def method1():
     Will print out the current DateTime, along with 
     the string 'Method 1 called.' """
 
-    log_file = open(log_file_name, "a")
+    log_file = open(log_file_absolute_path, "a")
 
     current_time = dt.datetime.now()
     out_string = str(current_time) + "  Method 1 called. \n"
