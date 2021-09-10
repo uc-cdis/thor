f571c53564439f4372fd60505b66c27c1c44303c
Marcelo Costa <themarcelor@gmail.com>
2021-09-10 14:32:00 -0500

    improve documentation to remove PYTHOHNPATH

--- src/thor/main.py
+++ src/thor/main.py
@@ -3,7 +3,7 @@
 import os
 import logging
 
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
@@ -13,21 +13,15 @@ from thor.dao.task_dao import read_task, get_task_keys
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 log = logging.getLogger(__name__)
 
-router = APIRouter()
+app = FastAPI(title="Thor Gen3 Release Orchestrator",)
 
 
-def get_app():
-    app = FastAPI(title="Thor Gen3 Release Orchestrator",)
-    app.include_router(router)
-
-    @app.on_event("startup")
-    async def startup_event():
-        print("scheduler should be initialized here...")
-
-    return app
+@app.on_event("startup")
+async def startup_event():
+    print("scheduler should be initialized here...")
 
 
-@router.get("/releases")
+@app.get("/releases")
 async def get_all_releases():
     """ Returns all the releases in the Releases table. """
     releases_to_return = []
@@ -40,7 +34,7 @@ async def get_all_releases():
     return JSONResponse(content={"releases": releases_to_return})
 
 
-@router.get("/releases/{release_id}")
+@app.get("/releases/{release_id}")
 async def get_single_release(release_id):
     """ Reads out the release associated with a particular release_id. """
     release = jsonable_encoder(read_release(release_id))
@@ -48,7 +42,7 @@ async def get_single_release(release_id):
     return {"release": release}
 
 
-@router.get("/releases/{release_id}/tasks")
+@app.get("/releases/{release_id}/tasks")
 async def get_all_release_tasks(release_id: int):
     """ This returns all tasks with release_id corresponding to the given input. 
     Currently, it fails without support if release_id is not an int, so this should
@@ -63,7 +57,7 @@ async def get_all_release_tasks(release_id: int):
     return tasks_to_return
 
 
-@router.get("/releases/{release_id}/tasks/{task_id}")
+@app.get("/releases/{release_id}/tasks/{task_id}")
 async def get_release_task_specific(release_id: int, task_id: int):
     """ This returns all tasks with release_id corresponding to the given input, and 
     task_id corresponding to the given input. Task_id is theoretically unique for each
@@ -81,7 +75,7 @@ async def get_release_task_specific(release_id: int, task_id: int):
     return tasks_to_return
 
 
-@router.get("/tasks")
+@app.get("/tasks")
 async def get_all_tasks():
     """ Returns all the tasks in the Tasks table. """
     tasks_to_return = []
@@ -94,7 +88,7 @@ async def get_all_tasks():
     return JSONResponse(content={"tasks": tasks_to_return})
 
 
-@router.get("/tasks/{task_id}")
+@app.get("/tasks/{task_id}")
 async def get_single_task(task_id):
     """ Reads out the task associated with a given task_id. """
     t = jsonable_encoder(read_task(task_id))
