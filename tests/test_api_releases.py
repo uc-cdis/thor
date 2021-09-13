b34fbf28e88d942a17b77110928196220ea91386
colinyaos <89102198+colinyaos@users.noreply.github.com>
2021-09-13 12:51:03 -0500

    reading from txt files to json now functional. Great success.

--- tests/test_api_releases.py
+++ tests/test_api_releases.py
@@ -1,90 +1,18 @@
 import pytest
 import json
-from fastapi.testclient import TestClient
 
-# This is a pretty delicate thing, any change could cause it to break
-# Is there a better way to make this work?
-import sys
+from fastapi.testclient import TestClient
 
 from thor.main import app
 
 client = TestClient(app)
 
 
-print("working")
-# Question: Would it be better to put the expected outputs into a separate file,
-# to avoid making the code harder to read?
-expected_output_for_get_releases = {
-    "releases": [
-        {"version": "2021.09", "result": "In Progress", "release_id": 3},
-        {"version": "2021.07", "result": "Completed", "release_id": 4},
-    ]
-}
+with open("release_test_data.txt", "r") as read_release_test:
+    expected_output_for_get_releases = json.load(read_release_test)
 
-expected_output_for_get_tasks = {
-    "tasks": [
-        {
-            "task_id": 1,
-            "task_name": "Create Release in JIRA",
-            "status": "success",
-            "release_id": 4,
-        },
-        {
-            "task_id": 2,
-            "task_name": "Cut integration branch",
-            "status": "success",
-            "release_id": 4,
-        },
-        {
-            "task_id": 3,
-            "task_name": "Update CI env with the latest integration branch",
-            "status": "success",
-            "release_id": 4,
-        },
-        {
-            "task_id": 4,
-            "task_name": "Generate release notes",
-            "status": "success",
-            "release_id": 4,
-        },
-        {
-            "task_id": 5,
-            "task_name": "Run Load Tests",
-            "status": "success",
-            "release_id": 4,
-        },
-        {
-            "task_id": 6,
-            "task_name": "Merge integration branch into stable and tag release",
-            "status": "success",
-            "release_id": 4,
-        },
-        {
-            "task_id": 7,
-            "task_name": "Mark gen3 release as released in JIRA",
-            "status": "success",
-            "release_id": 4,
-        },
-        {
-            "task_id": 8,
-            "task_name": "Create Release in JIRA",
-            "status": "success",
-            "release_id": 3,
-        },
-        {
-            "task_id": 9,
-            "task_name": "Cut integration branch",
-            "status": "success",
-            "release_id": 3,
-        },
-        {
-            "task_id": 10,
-            "task_name": "Update CI env with the latest integration branch",
-            "status": "in progress",
-            "release_id": 3,
-        },
-    ]
-}
+with open("task_test_data.txt", "r") as read_task_test:
+    expected_output_for_get_tasks = json.load(read_task_test)
 
 
 def test_read_releases():
