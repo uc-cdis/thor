877847fb20ff11314c7e331f1a771aa82d804cb1
colinyaos <89102198+colinyaos@users.noreply.github.com>
2021-09-13 18:00:16 -0500

    midway through fix. Reverting to older version to checkout api responses.

--- tests/test_api_get_all_release_tasks.py
+++ tests/test_api_get_all_release_tasks.py
@@ -12,7 +12,20 @@ with open("task_test_data.txt", "r") as read_task_test:
     expected_output_for_get_tasks = json.load(read_task_test)
 
 
-def test_get_all_release_tasks():
-    response = client.get("/tasks")
+@pytest.mark.parametrize("release_id", [3, 4])
+def test_get_all_release_tasks(release_id):
+    response = client.get(f"/releases/{release_id}/tasks")
     assert response.status_code == 200
-    assert response.json() == expected_output_for_get_tasks
+
+    list_of_desired_tasks = [
+        task
+        for task in expected_output_for_get_tasks["tasks"]
+        if task["release_id"] == release_id
+    ]
+
+    expected_output_for_get_all_release_tasks = (
+        f'{{ "release": {json.dumps(list_of_desired_tasks)} }}'
+    )
+
+    print(response.json)
+    assert response.json() == json.load(expected_output_for_get_all_release_tasks)
