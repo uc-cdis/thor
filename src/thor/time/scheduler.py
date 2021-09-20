1c30c040bbbbbff5c2cafdfd33840c5760532dc5
colinyaos <89102198+colinyaos@users.noreply.github.com>
2021-09-20 18:43:35 -0500

    Written scheduler - schedule_job_cron, etc. in quick session.

--- src/thor/time/scheduler.py
+++ src/thor/time/scheduler.py
@@ -34,54 +34,67 @@ class Scheduler:
 
     def initialize_scheduler(self):
         loop = asyncio.new_event_loop()
-        futures = []
 
         jjm = JenkinsJobManager()
 
         # Replace following block with method to schedule only the jobs with
         # fixed cron schedule.
 
-        for jk, jv in self.jobs_and_schedules.items():
-            log.info(f"iterating through job {jk}")
-            # debugging
-            time.sleep(1)
-            if jv["schedule"]:
-                job_reference = jjm.run_job
-                job_args = (jv["job_name"], jv["job_params"])
-                cron_input = jv["schedule"]
-                futures.append(
-                    self.schedule_job_cron(job_reference, job_args, cron_input, loop)
-                )
+        futures_dict_verbose = {}
+        for step, step_info_dict in self.jobs_and_schedules.items():
+            # step is the first-level job name - step1, step2, etc.
+            # info_dict is the dict that contains all the other interesting info
+            # info_dict is like {job_name: , job_params:{ } , schedule: , run_next: }
+
+            cron_input = step_info_dict["schedule"]
+
+            if cron_input:
+                future_job = self.schedule_job_cron(step_info_dict, loop)
+
+                futures_dict_verbose[step] = {
+                    "future_reference": future_job,
+                    "step_info": step_info_dict,
+                }
+
             else:
-                log.info(
-                    f"no schedule set for job {jk}, it must run immediately once the previous job is successful."
-                )
+                log.info(f"No schedule for step {step}")
+
+        # Now, we should have a complete futures_dict_verbose.
+        # This file should contain all the info that we need for the next stage of the job.
 
         # Replace following code with methods to call the Jenkins job and
         # keep polling Jenkins until we get a successful run of the newest job.
 
-        print("RUNNING LOUDLY")
+        for step_name, step_info in futures_dict_verbose.items():
 
-        for f in futures:
+            log.debug("time:", dt.datetime.now())
+            loop.run_until_complete(step_info["future_reference"])
 
-            print("waiting until", f, "\n", dt.datetime.now())
-            loop.run_until_complete(f)
+            log.debug(f"job step {step_name} is complete. ")
 
-            print(f)
-            # jjm.check_result_of_job()
+            job_name = step_info["step_info"]["job_name"]
+            expected_version = "2021.09"  # Temporary for testing purposes
 
-            if f.done():
-                log.info(f"### ## job {str(f)} executed as per schedule.")
+            while True:
+                time.sleep(5)
+                result = JenkinsJobManager().check_result_of_job(
+                    job_name, expected_version
+                )
 
-    def schedule_job_cron(self, job_reference, job_args, cron_input, loop):
+            print(result)
+
+    def schedule_job_cron(self, step_info_dict, loop):
         """ Schedules a job with a cron_input """
 
+        job_reference = JenkinsJobManager().run_job
+        job_name = step_info_dict["job_name"]
+        job_params = step_info_dict["job_params"]
+        schedule = step_info_dict["schedule"]
         # assemble job_name for logging purposes
-        job_name = job_args[0]
 
         # snippet of code from
         # https://python.hotexamples.com/examples/aiocron/-/crontab/python-crontab-function-examples.html
-        t = crontab(cron_input, func=job_reference, args=job_args, loop=loop)
+        t = crontab(schedule, func=job_reference, args=job_params, loop=loop)
         log.info(f"successfully scheduled job {job_name}.")
 
         future = asyncio.ensure_future(t.next(), loop=loop)
