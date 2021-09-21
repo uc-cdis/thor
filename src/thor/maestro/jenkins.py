a94ca77a72ab596c29a4e013f18ee8474ac0717f
colinyaos <89102198+colinyaos@users.noreply.github.com>
2021-09-20 19:09:55 -0500

    messy fixes continue

--- src/thor/maestro/jenkins.py
+++ src/thor/maestro/jenkins.py
@@ -2,17 +2,19 @@ import os
 import logging
 import requests
 import json
-
-# import jenkins
+import datetime
 
 from thor.maestro.baton import JobManager
+from thor.dao.task_dao import create_task, lookup_task_key, update_task
+from thor.dao.release_dao import release_id_lookup_class
+
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 log = logging.getLogger(__name__)
 
 
 class JenkinsJobManager(JobManager):
-    def __init__(self, base_jenkins_url="https://jenkins2.planx-pla.net/job", **kwargs):
+    def __init__(self, base_jenkins_url="jenkins2.planx-pla.net", **kwargs):
         """
     Creates Jenkins Job Manager API client
     """
@@ -27,46 +29,19 @@ class JenkinsJobManager(JobManager):
             # use parent default
             super().__init__()
 
-    def run_job(self, job_name, job_parameters, job_token):
-        """This function takens in a job_name and job parameters to remotely trigger a Jenkins job.
-            It further calls another function, assemble_url, that transforms the parameters dictionary 
-            into a buildWithParameters URL. To achieve this, the function concatenates an empty string with
-            the return value of assemble_url. An exception is raised if the concatenation fails."""
+    def run_job(self, job_name, job_parameters):
         log.info(f"running jenkins job {job_name}")
         log.info(f"parameters {job_parameters}")
-        auth = (self.jenkins_username, self.jenkins_api_token)
-        try:
-            # TODO: improve url assembly validation
-            full_url = self.assemble_url(job_name, job_parameters, job_token)
-            response = requests.post(full_url, auth=auth)
-            # this request should operate like the curl command below
-            # curl -L -s -o /dev/null -w "%{http_code}" -u user:$JENKINS_API_TOKEN "http://localhost:6579/job/this-is-a-test/buildWithParameters?token=<your_job_secret_token>&THE_NAME=William&RELEASE_VERSION=2021.09"
-            log.debug(f"reponse status code: {response.status_code}")
-            # Capture parameters by reading the metadata of the job_name and see if it matches the keys of the dict.
-
-        except requests.exceptions.HTTPError as httperr:
-            log.error(
-                f"request to {self.base_jenkins_url}/{job_name} failed due to the following error: {e}"
-            )
-
-    def assemble_url(self, job_name, job_parameters, job_token):
-        """Takes in job_parameters to assemble a URL that transforms the parameters dictionary 
-        into a buildWithParameters URL containing the sequence of parameter key and value."""
-        url = (
-            f"{self.base_jenkins_url}/{job_name}/buildWithParameters?token={job_token}"
-        )
-        for k, v in job_parameters.items():
-            url += f"&{k}={v}"
-        # omitting the token from the url
-        sanitized_url = (
-            url[0 : int(url.index("token"))] + url[url.index("&") : len(url)]
-        )
-        log.debug(f"the url has been assembled: {sanitized_url}")
-        return url
+        log.info(f"job invoked at: {datetime.datetime.now()}")
+        # TODO: Capture parameters by reading the metadata of the job_name and see if it matches the keys of the dict
+        # TODO: write python code to execute something like this
+        # curl -L -s -o /dev/null -w "%{http_code}" -u user:$JENKINS_API_TOKEN "http://localhost:6579/job/this-is-a-test/buildWithParameters?token=<your_job_secret_token>&THE_NAME=William&RELEASE_VERSION=2021.09"
+        return f"result from {job_name}"
 
     def check_result_of_job(self, job_name, expected_release_version):
         release_version = "UNKNOWN"
         print(f"checking the results of the jenkins job {job_name}")
+        log.info(f"Checking results of jenkins job {job_name}. ")
         url = f"https://{self.base_jenkins_url}/job/{job_name}/lastBuild/api/json"
         jsonOutput = None
         try:
@@ -85,9 +60,18 @@ class JenkinsJobManager(JobManager):
             # get result
             result = json.loads(jsonOutput)["result"]
             print(f"### ##The result for the lasted build is {result}")
+            log.info(
+                f"Latest version of {job_name} is {release_version}, and the result is {result}. "
+            )
+
         except Exception as e:
             print(f"response: {jsonOutput}")
             print(f"### ## Something went wrong: {e}")
+            log.info(
+                f"Encountered error when accessing {job_name}: \n \
+                Exception: {e} \n \
+                    Full response: {jsonOutput}. "
+            )
 
         if release_version == expected_release_version:
             return result
@@ -123,12 +107,16 @@ class JenkinsJobManager(JobManager):
             create_task(job_name, result, corresponding_release_id)
 
 
-"""#1 is to check for fork main"""
 if __name__ == "__main__":
     jjm = JenkinsJobManager()
-    paramsDict = {
-        "RELEASE_VERSION": "2021.12",
-        "THE_NAME": "William",
-    }
+    # paramsDict = {
+    #     "RELEASE_VERSION": "2021.09",
+    #     "FORK_FROM": "main",
+    # }
+    # jjm.run_job("say-hello", paramsDict)
+    # print("\nCHECKRESULT\n", jjm.check_result_of_job("say-hello", "2021.09"), "\n\n")
 
-    jjm.run_job("say-hello", paramsDict, os.environ["JENKINS_JOB_TOKEN"])
+    test_task_name = "Update CI env with the latest integration branch"
+    test_task_version = "2021.09"
+
+    jjm.write_task_result(test_task_name, test_task_version)
