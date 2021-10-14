21d17fb8185a6fdac9952694436f0f424c75fc36
jingh8 <jingh8@uchicago.edu>
2021-10-14 12:56:27 -0500

    add poetry path

--- src/thor/startup_script.sh
+++ src/thor/startup_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # start scheduler
-poetry run python src/thor/time/scheduler.py &
+$HOME/.poetry/bin/poetry run python src/thor/time/scheduler.py &
 
 # start web server
-poetry run gunicorn thor.main:app -b 0.0.0.0:6565 -k uvicorn.workers.UvicornWorker --reload
+$HOME/.poetry/bin/poetry run gunicorn thor.main:app -b 0.0.0.0:6565 -k uvicorn.workers.UvicornWorker --reload
