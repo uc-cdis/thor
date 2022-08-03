4e02c4a851f175a1ed30d8b3a708461a2f515e02
haraprasadj <haraprasadj@uchicago.edu>
2022-08-03 09:49:55 -0700

    create release tasks in step 1

--- jenkins-jobs-scripts/step1/create-release-in-jira.sh
+++ jenkins-jobs-scripts/step1/create-release-in-jira.sh
@@ -7,4 +7,4 @@ export CRYPTOGRAPHY_DONT_BUILD_RUST=1
 
 poetry install
 poetry run python3 jenkins-jobs-scripts/step1/create-release-in-jira.py
-
+poetry run python3 jenkins-jobs-scripts/step1/create-release-tasks-in-jira.py
