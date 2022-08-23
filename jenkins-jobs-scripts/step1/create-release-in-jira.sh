5bae93e7e1fb6d02f1d2a9e3d7d2fc7261a1ffdc
haraprasadj <haraprasadj@uchicago.edu>
2022-08-23 08:12:06 -0700

    fixing

--- jenkins-jobs-scripts/step1/create-release-in-jira.sh
+++ jenkins-jobs-scripts/step1/create-release-in-jira.sh
@@ -6,4 +6,4 @@ pip install --upgrade pip
 export CRYPTOGRAPHY_DONT_BUILD_RUST=1
 
 /env/bin/python /src/jenkins-jobs-scripts/step1/create-release-in-jira.py
-/env/bin/python /src/jenkins-jobs-scripts/step1/create-release-tasks-in-jira.py
+# /env/bin/python /src/jenkins-jobs-scripts/step1/create-release-tasks-in-jira.py
