9d6bd7f80937f72b1c37e7ec285a8aca629863e8
haraprasadj <haraprasadj@uchicago.edu>
2022-08-23 01:15:43 -0700

    use requests

--- jenkins-jobs-scripts/step1/create-release-in-jira.py
+++ jenkins-jobs-scripts/step1/create-release-in-jira.py
@@ -3,6 +3,9 @@ import re
 import os
 import sys
 import datetime
+import requests
+from requests.auth import HTTPBasicAuth
+import json
 
 options = {"server": "https://ctds-planx.atlassian.net"}
 jira = JIRA(
@@ -11,15 +14,36 @@ jira = JIRA(
 
 monthinteger = int(os.environ["RELEASE_VERSION"].split(".")[1])
 month = datetime.date(1900, monthinteger, 1).strftime("%B")
+year = os.environ["RELEASE_VERSION"].split(".")[0]
 
-result = jira.create_version(
-    "{}".format(os.environ["RELEASE_VERSION"]),
-    os.environ["JIRA_PROJECT"],
-    description="[Thor] Gen3 Release - {}".format(month),
-    releaseDate=None,
-    startDate=None,
-    archived=False,
-    released=False,
+# result = jira.create_version(
+#     "{}".format(os.environ["RELEASE_VERSION"]),
+#     os.environ["JIRA_PROJECT"],
+#     description="[Thor] Gen3 Release - {} {}".format(month, year),
+#     releaseDate=None,
+#     startDate=None,
+#     archived=False,
+#     released=False,
+# )
+url = "https://ctds-planx.atlassian.net//rest/api/3/version"
+auth = HTTPBasicAuth(os.environ["JIRA_SVC_ACCOUNT"].strip(), os.environ["JIRA_API_TOKEN"].strip())
+headers = {
+   "Accept": "application/json",
+   "Content-Type": "application/json"
+}
+payload = json.dumps( {
+    "archived": False,
+    "releaseDate": None,
+    "name": os.environ["RELEASE_VERSION"],
+    "description": "[Thor] Gen3 Release - {}".format(month),
+    "projectId": os.environ["JIRA_PROJECT"],
+    "released": True
+} )
+res = requests.post(
+    url,
+    data=payload,
+    headers=headers,
+    auth=auth
 )
 
-print("result: {}".format(result))
+print(json.dumps(json.loads(res.text), sort_keys=True, indent=4, separators=(",", ": ")))
