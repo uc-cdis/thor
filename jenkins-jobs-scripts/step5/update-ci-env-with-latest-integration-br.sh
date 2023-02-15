5287317f89ae4d02ed8b817bd4608eb9fe122d59
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2023-02-14 22:34:20 -0800

    fix

--- jenkins-jobs-scripts/step5/update-ci-env-with-latest-integration-br.sh
+++ jenkins-jobs-scripts/step5/update-ci-env-with-latest-integration-br.sh
@@ -3,14 +3,15 @@ GITHUB_USERNAME="PlanxCyborg"
 GITHUB_TOKEN=${GITHUB_TOKEN//$'\n'/}
 
 git clone "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/uc-cdis/gen3-release-utils.git"
-git clone "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/uc-cdis/gitops-qa.git"
 
 cd gen3-release-utils/gen3release-sdk
 pip install -U pip
 pip install poetry
 poetry install
 
+git clone "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/uc-cdis/gitops-qa.git"
+
 IFS=';' read -ra ENVS <<< "$TARGET_ENVIRONMENTS"
 for ENV in "${ENVS[@]}"; do
-  poetry run gen3release apply -v $INTEGRATION_BRANCH -e ../gitops-qa/${ENV} -pr "${PR_TITLE} ${INTEGRATION_BRANCH} ${ENV} $(date +%s)"
+  poetry run gen3release apply -v $INTEGRATION_BRANCH -e ../../gitops-qa/${ENV} -pr "${PR_TITLE} ${INTEGRATION_BRANCH} ${ENV} $(date +%s)"
 done
