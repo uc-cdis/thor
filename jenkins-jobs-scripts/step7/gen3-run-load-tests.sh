a4230e2cfe4f3450f7818ab06b21f1a04d34a029
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2023-05-15 21:27:17 -0700

    Fix step 7

--- jenkins-jobs-scripts/step7/gen3-run-load-tests.sh
+++ jenkins-jobs-scripts/step7/gen3-run-load-tests.sh
@@ -2,6 +2,6 @@
 
 # Completely running gen3 load tests using Jenkins. 
 
-/env/bin/python jenkins-jobs-scripts/step7/gen3-run-load-tests.py &
+/env/bin/python /src/jenkins-jobs-scripts/step7/gen3-run-load-tests.py &
 pid = $!
 wait $pid
