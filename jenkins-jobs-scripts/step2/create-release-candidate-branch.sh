a5422164192b72558b115ff2f711c30d0b4c8ff3
Hara Prasad <haraprasadj@uchicago.edu>
2023-07-17 09:05:40 -0700

    Fix urlPrefix in step 2 (#93)
    * Fix urlPrefix in step 2
    
    * Synchronize repo
    
    * fix config and remove deadwood
    
    * more cleanup

--- jenkins-jobs-scripts/step2/create-release-candidate-branch.sh
+++ jenkins-jobs-scripts/step2/create-release-candidate-branch.sh
@@ -2,23 +2,9 @@
 export GITHUB_USERNAME="PlanXCyborg"
 export GITHUB_TOKEN=${GITHUB_TOKEN//$'\n'/}
 
-BRANCH_NAME=""
-YEAR_STR=""
-BRANCH_NAME=""
-if [[ $RELEASE_VERSION =~ ([0-9]{4})\.([0-9]{2}) ]]; then
-  echo "match"
-  YEAR_STR="${BASH_REMATCH[1]}"
-  CONVERTED_MONTH_STR_TO_NUMBER=$(expr ${BASH_REMATCH[2]} + 0)
-  BRANCH_NAME=$(printf "%02d\n" $CONVERTED_MONTH_STR_TO_NUMBER)
-  echo "creating branch integration${YEAR_STR}${BRANCH_NAME}..."
-else
-  echo "not match. Skip branch creation."
-  exit 1
-fi
-
-urlPrefix="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/"
-sourceBranchName="$FORK_FROM"
-targetBranchName="integration${YEAR_STR}${BRANCH_NAME}"
+urlPrefix="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/uc-cdis/"
+sourceBranchName=$FORK_FROM
+targetBranchName=$INTEGRATION_BRANCH
 
 if find . -name "gen3-integration" -type d; then
   echo "Deleting existing gen3-integration folder"
@@ -35,9 +21,9 @@ repo_list="/src/repo_list.txt"
 while IFS= read -r repo; do
   echo "### Cutting ${targetBranchName} branch for repo ${repo} ###"
   git clone "${urlPrefix}${repo}"
-  repo_folder=$(echo $repo | awk -F'/' '{print $2}')
-  echo "### stepping into ${repo_folder}..."
-  cd "${repo_folder}" || exit 1
+  echo "### stepping into ${repo} directory ..."
+  cd ${repo} || exit 1
+  git fetch
   git checkout "${sourceBranchName}"
   result=$(git checkout -b "${targetBranchName}")
   RC=$?
