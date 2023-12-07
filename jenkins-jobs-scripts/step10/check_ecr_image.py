191d32062d5cdeec18ad1f6fd50a557d5de06126
Atharva Rane <atharvar@uchicago.edu>
2023-12-07 17:15:13 -0500

    fixing the flow

--- jenkins-jobs-scripts/step10/check_ecr_image.py
+++ jenkins-jobs-scripts/step10/check_ecr_image.py
@@ -1,35 +1,37 @@
 import os
 import subprocess
-import boto3
+# import boto3
 
 release = os.environ.get("RELEASE_VERSION")
 failed_list = []
 
-ecr = boto3.client('ecr')
+# ecr = boto3.client('ecr')
 
-def get_ecr_image():
+def get_ecr_image(services):
     # command to run gen3 ecr check
+    print("--------------------------------")
     print(f"Checking ECR image for {services}...")
-    # get_image_command = ['gen3', 'ecr', 'describe-image', 'services', 'release']
-    # try: 
-    #     image = subprocess.run(get_image_command, shell=True, check=True)
-    #     if 'imageDetails' in image.stdout:
-    #         return
-    # except subprocess.CalledProcessError:
-    #     print(f"ECR image with tag '{release}' does not exist in ECR repository '{services}'")
-    #     failed_list.append(services)
-    try:
-        response = ecr.describe_images(
-            repositoryName='gen3/{services}',
-            imageIds=[{'imageTag': release}]
-        )
-        image_info = response['imageDetails'][0]
-        print(f"ECR image with tag '{release}' exists in repository 'gen3/{services}")
-        print(f"Image Tag: (image_info['imageTags'])")
-        return True
-    except ecr.exceptions.ImageNotFoundException:
-        print(f"ECR image with tag '{release}' does not exist in repository 'gen3/{services}")
-        return False
+    get_image_command = ['gen3', 'ecr', 'describe-image', services, release]
+    try: 
+        image = subprocess.run(get_image_command, capture_output=True, text=True)
+        if image.returncode == 0:
+            print(f"Image {release} exists in repository {services}")
+            print(f"Image Details : {image.stdout}")
+    except subprocess.CalledProcessError:
+        print(f"ECR image with tag '{release}' does not exist in ECR repository '{services}'")
+        failed_list.append(services)
+    # try:
+    #     response = ecr.describe_images(
+    #         repositoryName='gen3/{services}', ## cant accept / only accepts - or _
+    #         imageIds=[{'imageTag': release}]
+    #     )
+    #     image_info = response['imageDetails'][0]
+    #     print(f"ECR image with tag '{release}' exists in repository 'gen3/{services}")
+    #     print(f"Image Tag: (image_info['imageTags'])")
+    #     return True
+    # except ecr.exceptions.ImageNotFoundException:
+    #     print(f"ECR image with tag '{release}' does not exist in repository 'gen3/{services}")
+    #     return False
 
 # here
 # key : github repo name
@@ -47,10 +49,9 @@ print("Check if the Quay Images are ready")
 with open("../../repo_list.txt") as repoList:
     for repo in repoList:
         repo = repo.strip()
-        services = repo
         if repo in repo_dict:
             services = repo_dict[repo]
-            get_ecr_image()
+            get_ecr_image(services)
             continue
         elif repo == "sower-jobs":
             print("Iterating through the list of images for sower-jobs")
@@ -63,12 +64,14 @@ with open("../../repo_list.txt") as repoList:
             ]
             for sowerjob in sower_jobs:
                 services = sowerjob.strip()
-                get_ecr_image()
+                get_ecr_image(services)
                 continue
-        get_ecr_image()
+    else:
+        services = repo
+        get_ecr_image(services)
 
 print(f"List of repos that failed the check : {failed_list}")
 # if the failed_list contains any repo name
 # then the job should fail and print the list
-if len(failed_list) > 0:
-    raise Exception(f"The following services do not have the quay image for {release}: {failed_list}")
+if failed_list:
+    raise Exception(f"The following services do not have the ECR image for {release}: {failed_list}")
