f254c8c9eef2c0a95f579467d9cb0159cd7003de
Atharva Rane <atharvar@uchicago.edu>
2023-12-07 12:55:13 -0500

    update step10 script

---
+++ jenkins-jobs-scripts/step10/check_ecr_image.py
@@ -0,0 +1,60 @@
+import os
+import subprocess
+
+release = os.environ.get("RELEASE_VERSION")
+failed_list = []
+
+
+def get_ecr_image():
+    # command to run gen3 ecr check
+    print(f"Checking ECR image for {services}...")
+    get_image_command = ['gen3', 'ecr', 'describe-image', 'services', 'release']
+    try: 
+        image = subprocess.run(get_image_command, shell=True, check=True)
+        if 'imageDetails' in image.stdout:
+            return
+    except subprocess.CalledProcessError:
+        print(f"ECR image with tag '{release}' does not exist in ECR repository '{services}'")
+        failed_list.append(services)
+
+# here
+# key : github repo name
+# value : quay image build name
+repo_dict = {
+    "pelican": "pelican-export",
+    "docker-nginx": "nginx",
+    "gen3-fuse": "gen3fuse-sidecar",
+    "cloud-automation": "awshelper",
+    "ACCESS-backend": "access-backend",
+    "cdis-data-client": "gen3-client",
+}
+
+print("Check if the Quay Images are ready")
+with open("../../repo_list.txt") as repoList:
+    for repo in repoList:
+        repo = repo.strip()
+        services = repo
+        if repo in repo_dict:
+            services = repo_dict[repo]
+            get_ecr_image()
+            continue
+        elif repo == "sower-jobs":
+            print("Iterating through the list of images for sower-jobs")
+            sower_jobs = [
+                "metadata-manifest-ingestion",
+                "get-dbgap-metadata",
+                "manifest-indexing",
+                "download-indexd-manifest",
+                "batch-export",
+            ]
+            for sowerjob in sower_jobs:
+                services = sowerjob.strip()
+                get_ecr_image()
+                continue
+        get_ecr_image()
+
+print(f"List of repos that failed the check : {failed_list}")
+# if the failed_list contains any repo name
+# then the job should fail and print the list
+if len(failed_list) > 0:
+    raise Exception(f"The following services do not have the quay image for {release}: {failed_list}")
