008e84aaf91e2906d34f7856056e4511b7a99bd6
Atharva Rane <atharvar@uchicago.edu>
2023-12-18 11:33:05 -0500

    revert back to use ecr api boto3

--- jenkins-jobs-scripts/step10/check_ecr_image.py
+++ jenkins-jobs-scripts/step10/check_ecr_image.py
@@ -1,42 +1,38 @@
 import os
-import subprocess
-# import boto3
+# import subprocess
+import boto3
 
 release = os.environ.get("RELEASE_VERSION")
 failed_list = []
 
-subprocess.run(['git', 'clone', 'https://github.com/uc-cdis/cloud-automation.git'])
-
-# Source gen3setup.sh script
-subprocess.run(['source', './cloud-automation/gen3/gen3setup.sh'], shell=True, executable='/bin/bash')
-
-# ecr = boto3.client('ecr')
+ecr = boto3.client('ecr')
 
 def get_ecr_image(services):
     # command to run gen3 ecr check
-    print("--------------------------------")
-    print(f"Checking ECR image for {services}...")
-    get_image_command = ['gen3', 'ecr', 'describe-image', services, release]
-    try: 
-        image = subprocess.run(get_image_command, capture_output=True, text=True)
-        if image.returncode == 0:
-            print(f"Image {release} exists in repository {services}")
-            print(f"Image Details : {image.stdout}")
-    except subprocess.CalledProcessError:
-        print(f"ECR image with tag '{release}' does not exist in ECR repository '{services}'")
-        failed_list.append(services)
-    # try:
-    #     response = ecr.describe_images(
-    #         repositoryName='gen3/{services}', ## cant accept '/' only accepts '-' or '_'
-    #         imageIds=[{'imageTag': release}]
-    #     )
-    #     image_info = response['imageDetails'][0]
-    #     print(f"ECR image with tag '{release}' exists in repository 'gen3/{services}")
-    #     print(f"Image Tag: (image_info['imageTags'])")
-    #     return True
-    # except ecr.exceptions.ImageNotFoundException:
-    #     print(f"ECR image with tag '{release}' does not exist in repository 'gen3/{services}")
-    #     return False
+    # print("--------------------------------")
+    # print(f"Checking ECR image for {services}...")
+    # ecr_repository = f"gen3/{services}"
+    # get_image_command = ['aws', 'ecr', 'describe-image', '--repository-name', ecr_repository, '--image-ids', f'imageTag={release}']
+    # try: 
+    #     image = subprocess.run(get_image_command, capture_output=True, text=True)
+    #     if image.returncode == 0:
+    #         print(f"Image {release} exists in repository {services}")
+    #         print(f"Image Details : {image.stdout}")
+    # except subprocess.CalledProcessError:
+    #     print(f"ECR image with tag '{release}' does not exist in ECR repository '{services}'")
+    #     failed_list.append(services)
+    try:
+        response = ecr.describe_images(
+            repositoryName=f'gen3/{services}',
+            imageIds=[{'imageTag': release}]
+        )
+        image_info = response['imageDetails'][0]
+        print(f"ECR image with tag '{release}' exists in repository 'gen3/{services}")
+        print(f"Image Tag: (image_info['imageTags'])")
+        return True
+    except ecr.exceptions.ImageNotFoundException:
+        print(f"ECR image with tag '{release}' does not exist in repository 'gen3/{services}")
+        return False
 
 # here
 # key : github repo name
@@ -67,8 +63,8 @@ with open("../../repo_list.txt") as repoList:
                 "download-indexd-manifest",
                 "batch-export",
             ]
-            for sowerjob in sower_jobs:
-                services = sowerjob.strip()
+            for sower_job in sower_jobs:
+                services = sower_job.strip()
                 get_ecr_image(services)
                 continue
     else:
