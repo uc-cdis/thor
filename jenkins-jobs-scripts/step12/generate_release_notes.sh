0c57dee5d2bb7ce9e1ab1ac57092181603f173fe
Hara Prasad <haraprasadj@uchicago.edu>
2024-03-08 06:29:59 -0800

    Fix/step 12 (#107)
    * release notes generation code
    
    * fix
    
    * fix config
    
    * fix typos and add logs
    
    * fix

---
+++ jenkins-jobs-scripts/step12/generate_release_notes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+export GITHUB_USERNAME="PlanXCyborg"
+export GITHUB_TOKEN=${GITHUB_TOKEN//$'\n'/}
+org="uc-cdis"
+
+/env/bin/python /src/jenkins-jobs-scripts/step12/generate_release_notes.py
+
+if [ -d gen3-release-utils ]; then
+  echo "Removing exisitng gen3-release-utils directory ..."
+  rm -rf gen3-release-utils
+fi
+
+echo "---- Installing gen3-release-sdk ----"
+git clone "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/uc-cdis/gen3-release-utils.git"
+cd gen3-release-utils/gen3release-sdk
+pip install -U pip
+pip install poetry
+poetry install
+
+echo "---- Publishing the release notes ----"
+GITHUB_TOKEN="$GITHUB_TOKEN" poetry run gen3release notes -v ${RELEASE_VERSION} -f ../../gen3-release-notes.md ../../manifest.json
+echo "---- Done ----"
