49c4f137489e7593fa0e1ca34c22d3b87562a97c
Krishna Agarwal <159047652+krishnaa05@users.noreply.github.com>
2024-07-26 16:22:30 -0500

    Update slack_release_publish.py

--- jenkins-jobs-scripts/step13/slack_release_publish.py
+++ jenkins-jobs-scripts/step13/slack_release_publish.py
@@ -24,7 +24,6 @@ headers = {
 data = {
     "ref": "master",
     "inputs": {
-        "slack_urls": "https://hooks.slack.com/services/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",
         "title": f":tada: Gen3 Release {release_version} ({release_title}) is out :tada:",
         "message": f"Please find the release notes here - {release_url}"
     }
