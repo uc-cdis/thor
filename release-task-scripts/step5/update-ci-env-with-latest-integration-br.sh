e4386cd950b638ebdb1a3698b6af161d31fa6527
Krishna Agarwal <159047652+krishnaa05@users.noreply.github.com>
2025-08-15 12:33:40 -0500

    Changes for Updating CI Env with Latest Integration Branch (#130)
    * Changes for Updating CI Env with Latest Integration Branch
    
    * Resolving comment
    
    * Handle etl and revproxy
    
    * install jq
    
    * handle enabled from gen3-helm
    
    * Fix wts and commit msg
    
    * Handle enabled false in incoming manifest
    
    * handle missing image property if service is enabled
    
    * Update for etl
    
    * Changes for ETL and ssjdispatcher
    
    * Fixes
    
    * Minor changes

--- release-task-scripts/step5/update-ci-env-with-latest-integration-br.sh
+++ release-task-scripts/step5/update-ci-env-with-latest-integration-br.sh
@@ -1,16 +1,53 @@
 #!/bin/bash
 export GITHUB_USERNAME="PlanXCyborg"
 export GITHUB_TOKEN=${GITHUB_TOKEN//$'\n'/}
+export GEN3_GITOPS_PATH="$(pwd)/$REPO_NAME"
+export GEN3_HELM_PATH="$(pwd)/gen3-helm"
 
-git clone "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/uc-cdis/gen3-release-utils.git"
-git clone "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/uc-cdis/gitops-qa.git"
+git config --global user.name "${GITHUB_USERNAME}"
+git config --global user.email "cdis@uchicago.edu"
+git clone "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/uc-cdis/${REPO_NAME}"
+git clone "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/uc-cdis/gen3-helm"
 
-cd gen3-release-utils/gen3release-sdk
 pip install -U pip
 pip install poetry
 poetry install
 
 IFS=';' read -ra ENVS <<< "$TARGET_ENVIRONMENTS"
 for ENV in "${ENVS[@]}"; do
-  GITHUB_TOKEN="$GITHUB_TOKEN" poetry run gen3release apply -v $INTEGRATION_BRANCH -e ../../gitops-qa/${ENV} -pr "${PR_TITLE} ${INTEGRATION_BRANCH} ${ENV} $(date +%s)" -l "gen3-release"
+  export TARGET_ENV=$ENV
+  TIMESTAMP=$(date +%s)
+  PR_NAME="${PR_TITLE} ${RELEASE_VERSION} ${ENV} ${TIMESTAMP}"
+  SANITIZED_ENV="${TARGET_ENV//\//_}"
+  BRANCH_NAME="chore/apply_${IMAGE_TAG_VERSION}_to_${SANITIZED_ENV}_${TIMESTAMP}"
+  COMMIT_MSG="Updating $TARGET_ENV with $IMAGE_TAG_VERSION"
+  REPO_OWNER="uc-cdis"
+  BASE_BRANCH="master"
+
+  # PERFORM GIT OPERATIONS TO SWITCH TO NEW BRANCH and navigate to target env folder
+  cd $GEN3_GITOPS_PATH
+  pwd
+  git switch master
+  git fetch origin
+  git checkout -b "$BRANCH_NAME"
+  cd "${TARGET_ENV}/values/"
+
+  # Update the yaml files
+  poetry run python /src/release-task-scripts/step5/update-ci-env-with-latest-integration-br.py
+
+  # PUSH in the branch and create a PR
+  git add .
+  git commit -m "${COMMIT_MSG}"
+  git push origin "$BRANCH_NAME"
+
+  curl -u "$GITHUB_USERNAME:$GITHUB_TOKEN" \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Content-Type: application/json" \
+  "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/pulls" \
+  -d "$(jq -n \
+    --arg title "$PR_NAME" \
+    --arg head "$BRANCH_NAME" \
+    --arg base "$BASE_BRANCH" \
+    '{title: $title, head: $head, base: $base}')"
 done
