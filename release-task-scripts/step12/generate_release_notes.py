73fa54c9f8e074b28d3ec95e8e1e15b834c9308d
Krishna Agarwal <159047652+krishnaa05@users.noreply.github.com>
2026-08-11 13:54:27 -0500

    Update generate_release_notes.py

--- release-task-scripts/step12/generate_release_notes.py
+++ release-task-scripts/step12/generate_release_notes.py
@@ -55,7 +55,8 @@ def generate_release_notes(release_version):
 
     print("---- Compute start and end date ----")
     # use Saturday's date (12am) to exclude all commits of Friday, they are in the previous month's notes
-    start_date = str(get_second_friday(prev_prev_year, prev_prev_month) + datetime.timedelta(days=1))
+    # start_date = str(get_second_friday(prev_prev_year, prev_prev_month) + datetime.timedelta(days=1))
+    start_date = "2026-07-12"
     # use Saturday's date (12am) to include all commits of Friday
     end_date = str(get_second_friday(prev_year, prev_month) + datetime.timedelta(days=1))
     print(f"Start date - {start_date}")
